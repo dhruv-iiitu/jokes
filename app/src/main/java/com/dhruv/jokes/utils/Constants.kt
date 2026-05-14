e0054579529580527289143554f9ddbc1bf72cda
dhruvsharma <dhruv.sharma@yulu.bike>
2026-05-15 02:28:27 +0530

    actual url change

--- app/src/main/java/com/dhruv/jokes/utils/Constants.kt
+++ app/src/main/java/com/dhruv/jokes/utils/Constants.kt
@@ -1,4 +1,4 @@
 package com.dhruv.jokes.utils
 
 // 10.0.2.2 maps to the host machine when the app runs in the Android emulator.
-const val BASE_URL = "http://10.0.2.2:8080/"
+const val BASE_URL = "https://jokes-backend-production.up.railway.app/"
